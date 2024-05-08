b0c50c727a95f03ebaacd6fd56b57b72635459b1
xxNathalie <pettersson.nathalie@hotmail.se>
2024-05-08 15:58:11 +0200

    new commit

--- codeapp/utils.py
+++ codeapp/utils.py
@@ -15,8 +15,8 @@ def get_data_list() -> list[Jobs]:
     Function responsible for downloading the dataset from the source, translating it
     into a list of Python objects, and saving it to a Redis list.
     """
-    # TODO
-    pass
+   url: str = "https://onu1.s2.chalmers.se/datasets/AI_ML_jobs.json"
+    response = requests.get(url, timeout=30)
 
 
 def calculate_statistics(dataset: list[Jobs]) -> dict[int | str, int]:
